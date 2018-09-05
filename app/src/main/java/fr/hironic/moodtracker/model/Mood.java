8b0f4a84dcce3245d774cea3d2a948dfa85966e7
Gaalrauch <ironsquare@cegetel.net>
2018-09-05 17:09:00 +0200

    First implementation of main activity layout. Action bar removed.

---
+++ app/src/main/java/fr/hironic/moodtracker/model/Mood.java
@@ -0,0 +1,15 @@
+package fr.hironic.moodtracker.model;
+
+import android.graphics.Color;
+
+public class Mood {
+
+    private Color mMoodColor;
+
+    public Mood (int moodValue) {
+
+
+
+    }
+
+}
