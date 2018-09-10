916e1c3dad0d90ac42558b676295f5a6b0797539
Gaalrauch <ironsquare@cegetel.net>
2018-09-10 19:41:42 +0200

    Here is the Constants class

---
+++ app/src/main/java/fr/hironic/moodtracker/Constants.java
@@ -0,0 +1,19 @@
+package fr.hironic.moodtracker;
+
+public class Constants {
+
+    public static final String PREF_KEY_SHARED_KEY = "MOOD_TRACKER";
+    public static final String PREF_KEY_TODAY_NUMBER = "LAST_DATE";
+    public static final String PREF_KEY_TODAY_MOOD = "TODAY_MOOD";
+    public static final String PREF_KEY_TODAY_COMMENT = "TODAY_COMMENT";
+    public static final String PREF_MOOD_HISTORY = "MOOD_HISTORY";
+
+    public static final int DEFAULT_MOOD_VALUE = 3;
+    public static final int[] MOOD_COLORS = { R.color.faded_red,
+            R.color.warm_grey,
+            R.color.cornflower_blue_65,
+            R.color.light_sage,
+            R.color.banana_yellow
+    };
+
+}
