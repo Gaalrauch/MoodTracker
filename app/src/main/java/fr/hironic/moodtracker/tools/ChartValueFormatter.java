a225b20de44baa954c9691442c4c433ff706d768
Gaalrauch <ironsquare@cegetel.net>
2018-09-11 14:04:01 +0200

    Comment class ChartValueFormatter

--- app/src/main/java/fr/hironic/moodtracker/tools/ChartValueFormatter.java
+++ app/src/main/java/fr/hironic/moodtracker/tools/ChartValueFormatter.java
@@ -6,6 +6,11 @@ import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import java.text.DecimalFormat;
 
+/**
+ * Format pie chart values:
+ *  - replace "0" by an empty String ""
+ *  - format other values to decimal and add percentage symbol " %"
+ */
 public class ChartValueFormatter implements ValueFormatter {
 
     private DecimalFormat mFormat;
