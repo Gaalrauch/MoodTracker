f16cb3e3bc650cfd29c64600a127ad3c0c6ce782
Gaalrauch <ironsquare@cegetel.net>
2018-09-12 00:54:28 +0200

    Renamed calendar to userCalendar, improved class comments

--- app/src/main/java/fr/hironic/moodtracker/tools/DateManager.java
+++ app/src/main/java/fr/hironic/moodtracker/tools/DateManager.java
@@ -6,26 +6,26 @@ import java.util.TimeZone;
 public abstract class DateManager {
 
     /**
-     * Return the number of days since 1st January 1970
+     * Calculate the "day number", number of days since 1st January 1970
      * Create a calendar with default time zone to get the date in user's TimeZone
      * Create another calendar using GMT TimeZone
      * Set this new calendar to the date of the first calendar
-     * Return the number of days.
-     * @return
+     * Get the number of days since 1970 for this GMT calendar and return it.
+     * @return day number
      */
     public static int getTodayNumber() {
-        Calendar calendar = Calendar.getInstance(); // Get date from our TimeZone
-        calendar.set(calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH),
+        Calendar userCalendar = Calendar.getInstance(); // Get date from our TimeZone
+        userCalendar.set(userCalendar.get(Calendar.YEAR),
+                userCalendar.get(Calendar.MONTH),
+                userCalendar.get(Calendar.DAY_OF_MONTH),
                 12,
                 0,
                 0);
         Calendar gmtCalendar = Calendar.getInstance(TimeZone.getTimeZone("GMT")); // Get date from GMT TimeZone
-        // Set gmtCalendar to the same date calendar
-        gmtCalendar.set(calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH),
+        // Set gmtCalendar to the same date as userCalendar
+        gmtCalendar.set(userCalendar.get(Calendar.YEAR),
+                userCalendar.get(Calendar.MONTH),
+                userCalendar.get(Calendar.DAY_OF_MONTH),
                 12,
                 0,
                 0);
