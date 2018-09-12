99129e8f99c38182669fba9569c202973c6456f8
Gaalrauch <ironsquare@cegetel.net>
2018-09-12 18:23:21 +0200

    GetTodayNumber::getTodayNumber() use GMT offset instead of setting a GMT calendar to the same hour than user's calendar

--- app/src/main/java/fr/hironic/moodtracker/tools/GetTodayNumber.java
+++ app/src/main/java/fr/hironic/moodtracker/tools/GetTodayNumber.java
@@ -2,6 +2,7 @@ package fr.hironic.moodtracker.tools;
 
 import java.util.Calendar;
 import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Gaalrauch
@@ -12,30 +13,24 @@ import java.util.TimeZone;
 public abstract class GetTodayNumber {
 
     /**
-     * Calculate the "day number"
+     * Calculate the "day number, number of days since January 1st 1970
      * Create a calendar with default time zone to get the date in user's TimeZone
-     * Create another calendar using GMT TimeZone
-     * Set this new calendar to the date of the first calendar
-     * Get the number of days since 1970 for this GMT calendar and return it.
+     * Get GMT offset
+     * Set calendar hour to midday (to prevent random 1 missing millisecond)
+     * Get time in millis and add GMT Offset
+     * Calculate number of day
      * @return day number
      */
     public static int getTodayNumber() {
-        Calendar userCalendar = Calendar.getInstance(); // Get date from our TimeZone
-        userCalendar.set(userCalendar.get(Calendar.YEAR),
-                userCalendar.get(Calendar.MONTH),
-                userCalendar.get(Calendar.DAY_OF_MONTH),
+        Calendar calendar = Calendar.getInstance();
+        TimeZone timeZone = calendar.getTimeZone();
+        calendar.set(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
                 12,
                 0,
                 0);
-        Calendar gmtCalendar = Calendar.getInstance(TimeZone.getTimeZone("GMT")); // Get date from GMT TimeZone
-        // Set gmtCalendar to the same date as userCalendar
-        gmtCalendar.set(userCalendar.get(Calendar.YEAR),
-                userCalendar.get(Calendar.MONTH),
-                userCalendar.get(Calendar.DAY_OF_MONTH),
-                12,
-                0,
-                0);
-
-        return (int) (gmtCalendar.getTimeInMillis() / 86400000);
+        long millis = calendar.getTimeInMillis() + timeZone.getRawOffset();
+        return (int) (millis / 86400000);
     }
 }
